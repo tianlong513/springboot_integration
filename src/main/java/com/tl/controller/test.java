40a6e763d8162853c8cc8734275351a1a426fe9d
tianlong513 <44767147+tianlong513@users.noreply.github.com>
2018-12-10 14:36:38 +0800

    添加了测试controller

---
+++ src/main/java/com/tl/controller/test.java
@@ -0,0 +1,18 @@
+package com.tl.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @program: tl
+ * @description:
+ * @author:
+ * @create: 2018-12-10 14:32
+ **/
+@RestController
+public class test {
+    @RequestMapping(value = "hello")
+    public String Hello() {
+        return "hello1";
+    }
+}
