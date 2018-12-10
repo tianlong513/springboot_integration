5bbd151ed94a75b5af97250d01d024a55661f87a
tianlong513 <44767147+tianlong513@users.noreply.github.com>
2018-12-10 16:31:27 +0800

    创建UserInfo实体类

---
+++ src/main/java/com/tl/entity/UserInfo.java
@@ -0,0 +1,73 @@
+package com.tl.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+/**
+ * @program: tl
+ * @description: 用户实体类
+ * @author:
+ * @create: 2018-12-10 16:19
+ **/
+
+@Entity
+@Table(name = "user_info")
+public class UserInfo {
+    @Id
+    private String id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "phone")
+    private Integer phone;
+
+    @Column(name = "create_date")
+    private Date createDate;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Integer phone) {
+        this.phone = phone;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+}
